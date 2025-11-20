be0146835203b8f3edc29f24a7aa2a1123232dcf
MrNemod <coli-avatar-2002@hotmail.com>
2025-11-20 17:51:44 -0600

    catalogo-rthg.onrender.com agregado en allowed hosts

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -31,7 +31,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
-                 'https://catalogo-rthg.onrender.com',]
+                 'https://catalogo-rthg.onrender.com',
+                 'catalogo-rthg.onrender.com',]
 
 
 # Application definition
