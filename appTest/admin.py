89bc70f3069652b7ea38de2bf485026b126d377f
MrNemod <coli-avatar-2002@hotmail.com>
2025-11-22 22:16:44 -0600

    BaseModel agregado para heredar a otros

--- appTest/admin.py
+++ appTest/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import solarHeater, solarHeaterImage
+from .models import solarHeater, solarLight, productImage
 
 class SolarHeaterImageInline(admin.TabularInline):
-    model = solarHeaterImage
+    model = productImage
+    extra = 1
+
+class solarLightImageInline(admin.TabularInline):
+    model = productImage
     extra = 1
 
 @admin.register(solarHeater)
 class SolarHeaterAdmin(admin.ModelAdmin):
     inlines = [SolarHeaterImageInline]
+
+@admin.register(solarLight)
+class SolarLightAdmin(admin.ModelAdmin):
+    inlines = [solarLightImageInline]
--- appTest/models.py
+++ appTest/models.py
@@ -3,27 +3,53 @@ from .storage_backend import ImageKitStorage
 
 # Create your models here.
 
-class solarHeater(models.Model):
+class base_model(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Nombre")
+    description = models.TextField(blank=False, null=False, verbose_name="Descripcion")
+    price = models.IntegerField(blank=False, null=False, verbose_name="Precio")
+    brand = models.CharField(max_length=100, blank=False, null=False, verbose_name="Marca")
+
     class Meta:
-        verbose_name_plural = 'Productos'
-        verbose_name = 'Producto'
-    name = models.CharField(max_length=100,null=False, blank=False, verbose_name="Nombre")
-    description = models.TextField( blank=False, null=False, verbose_name="Descripcion")
-    price = models.IntegerField( blank=False, null=False, verbose_name="Precio")
-    liters = models.IntegerField( blank=False, null=False, verbose_name="Litros")
-    persons = models.IntegerField( blank=False, null=False, verbose_name="Personas")
-    number_of_tubes = models.IntegerField( blank=False, null=False, verbose_name="Numero de tubos")
-    brand = models.CharField(max_length=100, blank=False, null=False,  verbose_name="Marca")
+        abstract = False
 
     def __str__(self):
         return self.name
 
-class solarHeaterImage(models.Model):
-    heater = models.ForeignKey(
-        solarHeater,
+class solarHeater(base_model):
+    liters = models.IntegerField( blank=False, null=False, verbose_name="Litros")
+    persons = models.IntegerField( blank=False, null=False, verbose_name="Personas")
+    number_of_tubes = models.IntegerField( blank=False, null=False, verbose_name="Numero de tubos")
+
+    class Meta:
+        verbose_name_plural = 'Calentadores'
+        verbose_name = 'Calentador'
+
+    def __str__(self):
+        return self.name
+
+class solarLight(base_model):
+    watts = models.IntegerField( blank=False, null=False, verbose_name="Watts")
+    duration = models.IntegerField( blank=False, null=False, verbose_name="Duracion")
+    class Meta:
+        verbose_name_plural = 'Lamparas solares'
+        verbose_name = 'Lampara solar'
+
+# Agregar productos antes de esta linea
+class productImage(models.Model):
+    product = models.ForeignKey(
+        base_model,
         related_name='images',
         on_delete=models.CASCADE,
+        null=False,
+        blank=False,
     )
-    image = models.ImageField(storage=ImageKitStorage, blank=False, null=False,  verbose_name="Imagen(es)")
+
+    image = models.ImageField(
+        storage=ImageKitStorage,
+        blank=False,
+        null=False,
+        verbose_name="Imagen"
+    )
+
     def __str__(self):
-        return f'Imagen de {self.heater.name}'
+        return f'Imagen de {self.product.name}'
